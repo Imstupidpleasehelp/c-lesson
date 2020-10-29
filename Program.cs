e5cf87db4b2b5d72864e2449f75fe1f583d5ff0d
Dylan <dylanabozarth@gmail.com>
2020-10-28 20:41:16 -0700

    downloading files

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
+using System.Net;
+using System.Linq;
+using System.Text.RegularExpressions;
 namespace c_
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        WebClient webClient = new WebClient();
+    //download the file 
+webClient.DownloadFile("https://jsonplaceholder.typicode.com/users", "test.txt" );
+string JSONfile = System.IO.File.ReadAllText("test.txt");
+Console.WriteLine(JSONfile);
         }
     }
 }
